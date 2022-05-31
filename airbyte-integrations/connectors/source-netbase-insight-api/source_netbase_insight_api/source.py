8a300eb053e74c2bd30b9af169398c414a1f450d
Yvonne Huang <yhuang@netbase.com>
2022-05-31 16:56:58 +0800

    Incremental sync works

--- airbyte-integrations/connectors/source-netbase-insight-api/source_netbase_insight_api/source.py
+++ airbyte-integrations/connectors/source-netbase-insight-api/source_netbase_insight_api/source.py
@@ -7,8 +7,9 @@ from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
 import requests
+import copy
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream
 from requests.auth import HTTPBasicAuth, AuthBase
 
@@ -126,7 +127,7 @@ class IncrementalNetbaseInsightApiStream(NetbaseInsightApiStream, ABC):
         return {}
 
 
-class Topics(IncrementalNetbaseInsightApiStream):
+class Topics(IncrementalNetbaseInsightApiStream, IncrementalMixin):
     """
     TODO: Change class name to match the table/data source this stream corresponds to.
     """
@@ -168,52 +169,13 @@ class Topics(IncrementalNetbaseInsightApiStream):
     def state(self, value: Mapping[str, Any]):
         self.max_recorded_topicId = value[self.cursor_field]
 
-    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+    def read_records(self, stream_state: Mapping[str, Any] = None, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        initial_state = copy.deepcopy(stream_state) or {self.cursor_field: 0}
         for record in super().read_records(*args, **kwargs):
             current_topicId = int(record[self.cursor_field])
-            self.max_recorded_topicId = max(self.max_recorded_topicId, current_topicId)
-            yield record
-
-
-class Employees(IncrementalNetbaseInsightApiStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+            if current_topicId > initial_state.get(self.cursor_field):
+                self.max_recorded_topicId = max(self.max_recorded_topicId, current_topicId)
+                yield record
 
 
 # Source
